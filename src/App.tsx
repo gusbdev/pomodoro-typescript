9faf34f685da9c674b7ce18b1caf694c8f0e6b1c
Gus <gusbdev@gmail.com>
2021-08-26 21:22:22 -0300

    Use pomodoro component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { PomodoroTimer } from './components/pomodoro-times';
+
 function App(): JSX.Element {
-  return <div className="App">Teste</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
 }
 
 export default App;
